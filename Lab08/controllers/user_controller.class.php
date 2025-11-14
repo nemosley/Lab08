b0a886477aa3ecd32eefd79a7b90f1049a6c1043
Ben-Eetee <bjeggert@iu.edu>
2025-11-13 20:47:13 -0500

    added user controller class

---
+++ Lab08/controllers/user_controller.class.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Author: Benjamin Egger-Torke
+ * Date: 11/13/25
+ * File: user_controller.class.php
+ * Description:
+ **/
+
+class UserController
+{
+    private UserModel $userModel;
+
+    public function __construct()
+    {
+        $this->userModel = new UserModel();
+    }
+
+    public function index()
+    {
+        $view = new Index();
+        $view->display();
+    }
+
+    public function register()
+    {
+        $data = $this->userModel->add_user();
+        $view = new Register();
+        $view->display($data);
+
+    }
+
+    public function login()
+    {
+        $view = new Login();
+        $view->display();
+    }
+
+    public function verify()
+    {
+        $user = $this->userModel->verify_user();
+
+        $view= new VerifyUser();
+        $view->display($user);
+    }
+
+    public function logout()
+    {
+        $logout= $this->userModel->logout();
+        if(!$logout){
+            $message = "There was an error logging you in. Please try again.";
+            $log = false;
+        }
+        else {
+            $message = "You have successfully logged out.";
+            $log = true;
+        }
+        $view = new Logout();
+        $view->display($message, $log);
+    }
+
+    public function reset()
+    {
+        $view = new Reset();
+        $view->display();
+    }
+
+    public function do_reset()
+    {
+        $result = $this->userModel->reset_password();
+        $view = new ResetConfirm;
+        $view->display($result);
+    }
+
+    public function error($message)
+    {
+        $view = new Error();
+        $view->display($message);
+    }
+}
