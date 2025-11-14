41224b48593d70dc5619da72f0d935a8b31f813a
charleycorneil <146792079+charleycorneil@users.noreply.github.com>
2025-11-13 21:05:56 -0500

    added comments to view.class
    Refactor footer method to properly close the wrapper div and finish the HTML structure.

--- Lab08/views/view.class.php
+++ Lab08/views/view.class.php
@@ -15,20 +15,38 @@ class View {
         <html>
             <head>
                 <meta charset="UTF-8">
+                 <!-- Page title shown in browser tab -->
                 <title>PeaPOD User Management System</title>
+
+                <!-- Link to external stylesheet -->
                 <link href="www/css/styles.css" rel="stylesheet" type="text/css"/>
             </head>
-            <body>
-                <h1><span style="color: forestgreen; font-family: serif; font-size: 36pt">PeaPOD</span> User Management System</h1>
-                <div id="wrapper">
-                    <img src="www/img/peapod_logo.png" style="float: right; width: 130px">
-                    <?php
-                }
 
-                //create the page footer
-                public static function footer(): void {
-                    ?>
-                </div>
+            <body>
+
+                <!-- Application Title Banner -->
+                <h1>
+                    <span style="color: forestgreen; font-family: serif; font-size: 36pt">PeaPOD</span>
+                    User Management System
+                </h1>
+
+                <!-- Open wrapper for page content -->
+                <div id="wrapper">
+
+                    <!-- PeaPOD logo displayed on right side -->
+                    <img src="www/img/peapod_logo.png" style="float: right; width: 130px">
+
+        <?php
+    }
+
+    /**
+     * footer()
+     * Close wrapper div and finish HTML page.
+     */
+    public static function footer(): void {
+        ?>
+                </div>  <!-- End wrapper -->
+
             </body>
         </html>
         <?php
